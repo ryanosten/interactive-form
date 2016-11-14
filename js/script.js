6ea0ff456d766d81924b466663b21b91c36f2f8d
Ryan Osten <ostenrj@gmail.com>
2016-11-14 08:37:21 -0500

    minor enhancements

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
 	
+	//add total price element for activities
 	$('.activities label').last().after('<h2 class="total-price"></h2>');
 	$('.total-price').hide();
 
+	//focus on #name input on page load
+	$('#name').focus();
+
+	//function to sum the price of selected activities
 	function displayPrice(totalPrice){
 		$('.total-price').show();
 		$('.total-price').html('Price: ' + totalPrice);
 	}	
 
+	//function to check the conflict times of activities
 	function checkConflicts(activity){
 			
 			if(activity.attr('name') == "js-frameworks"){
@@ -28,6 +34,7 @@ $(document).ready(function() {
 		}
 	}
 
+	//reg expression function to validate email
 	function validateEmail(mail){  
  		if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){  
     		return true 
@@ -35,6 +42,7 @@ $(document).ready(function() {
     		return false 
 	}
 
+	//function that allows only numbers in inputs on which it is called. 
 	function numbersOnly(e){
 		//Only allow numbers
 		if(e.which != 8 && e.which != 0 && (e.which <48 || e.which > 57)){
@@ -42,48 +50,40 @@ $(document).ready(function() {
 		}
 	}  
 
-	//validate credit card
-
-	/*
-	function validCC(){
-		$('#cc-num').validateCreditCard(function(result){
-			result.valid;
-		});
-	}
-	*/
-
-	//focus on #name input on page load
-	$('#name').focus();
-
 	//bind a change event to the #title select list
 	$('#title').on("change", function(){
 		
-		//in the title list, if the option element has text of 'other' then show text input
+		//in the title list, if the option element has text of 'other' then show text input, else hide the input
 		if ($('option[value=other]').is(':selected')){
 			
-			//if option of 'other not selected', hide the input
+			//if option of 'other' selected, show the input 
 			$('#other-title').show();
 			
 		} else {
-
+			//else hide the 'other' input
 			$('#other-title').hide();
 		}
 	
 	});
 
-	//T-SHIRT DESIGN
+	//------------T-SHIRT DESIGN-----------------
 
+	//change event handler on design select menu
 	$('#design').on("change", function(){
 		
+		//on change show all select options
 		$('label[for=color], #color, #color option, #color-styled').show();
 		
+		//if 'select theme' is selected, hide color select menu
 		if($('#design').children().first().is(':selected')){
 			$('label[for=color], #color, #color-styled').hide();
 		
+		//if js puns is selected, change the value of color menue to cornflower blue and dlice and hide all non-js-puns colors
 		} else if ($('#design option[value="js puns"]').is(':selected')){
 			$('#color').val('cornflowerblue');
 			$('#color').children().slice(3).hide();
 
+		//if I <3 js selected, then hide all non-I<3js color and show the I <3 js colors
 		} else {
 			$('option[value=select-shirt]').hide()
 			$('#color').val('tomato');
@@ -92,10 +92,11 @@ $(document).ready(function() {
 
 	});
 	
+	//Click handler to calculate the price sum of activities activities selected
 	$('.activities input[type=checkbox]').on('click', function(){
 		
 		var totalPrice = 0;
-
+		
 		$('.activities label').css('color', 'black');
 		$('.activities input[type=checkbox]').removeAttr('disabled');
 
@@ -183,12 +184,15 @@ $(document).ready(function() {
 
 	$('button[type=submit]').on('click', function(){
 
+		//store object returned by validateCreditCard(), which is function from cardValidator.js
+		var ccResult = $('#cc-num').validateCreditCard()
+		
 		//create variable to store e-mail input for validation
 		var mailAdd = $('#mail').val();
 
 		//check if any of the input validators throw error. If yes, check which validation fails and then return false so that form does not submit. Reason we need to wrap all conditions in this larger if statement is because if we were to do otherwise and return false in each condition, then the following condition expressions wouldn't run after return false. 
 		if(!$('#name').val() || validateEmail(mailAdd) == false || (!$('#design option[value="js puns"]').is(':selected') && !$('#design option[value="heart js"]').is(':selected')) 
-			|| ($('#payment option[value="credit card"]').is(':selected') || $('#payment option[value="select_method"]').is(':selected') && (/*validCC() == false ||*/ !$('#cc-num').val() || ($('#zip').val() < 5) || $('#cvv').val().length < 3))){
+			|| ($('#payment option[value="credit card"]').is(':selected') && (!ccResult.valid || ($('#zip').val() < 5) || $('#cvv').val().length < 3))){
 
 			if(!$('#name').val()){
 				nameEmpty.show();
@@ -222,45 +226,27 @@ $(document).ready(function() {
 				activityEmpty.hide();
 			}
 
-			//check if card # empty
-			if(($('#payment option[value="credit card"]').is(':selected') || $('#payment option[value=select_method]').is(':selected')) && !$('#cc-num').val()){
+			//check if card valid
+			if($('#payment option[value="credit card"]').is(':selected') && !ccResult.valid){
 				$('label[for=cc-num]').css('color', 'red');
 			} else {
-				$('label[for=cc-num]').css('color', 'black')
+				$('label[for=cc-num]').css('color', 'black');
 			}
 
 			//check if zip empty
-			if(($('#payment option[value="credit card"]').is(':selected') || $('#payment option[value=select_method]').is(':selected')) && ($('#zip').val() < 5)){
+			if($('#payment option[value="credit card"]').is(':selected') && ($('#zip').val() < 5)){
 				$('label[for=zip]').css('color', 'red');
 			} else {
 				$('label[for=zip]').css('color', 'black');
 			}
 
 			var cvv = $('#cvv').val();
-			if(($('#payment option[value="credit card"]').is(':selected') || $('#payment option[value=select_method]').is(':selected')) && ($('#cvv').val().length < 3) /*|| isNaN(cvv)*/){
+			if($('#payment option[value="credit card"]').is(':selected') && ($('#cvv').val().length < 3)){
 				$('label[for=cvv]').css('color', 'red');
 			} else {
 				$('label[for=cvv]').css('color', 'black');
-			}
-
-
-			//check 
-			
-
-
-			/*
-			if (($('#payment option[value="credit card"]').is(':selected') || $('#payment option[value=select_method]').is(':selected')) && (validCC() == false)){
-				console.log('this shit run');
-				/*		
-				var validCC = ('<p> please enter a valid credit card number<p>');
-				$('#payment').after(validCC);
-				$(validCC).css('color', 'red');
-				
-			}
-
-			*/
-
-
+			}	
+		
 			return false
 		};
 	});
